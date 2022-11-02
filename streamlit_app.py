ac4caceedec7c2f126a0f2b3e57cd5ef0d8198a9
Yolanda <116392247+yolandadhoward28@users.noreply.github.com>
2022-11-02 16:08:37 -0500

    Update streamlit_app.py
    kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # streamlit.text(fruityvice_response.json()) # This just writes the text to the screen
 
 
