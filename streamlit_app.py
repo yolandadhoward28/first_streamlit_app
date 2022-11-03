e32818c8d8ae108be69ebb2d021002dfe0ea4028
Yolanda <116392247+yolandadhoward28@users.noreply.github.com>
2022-11-03 10:31:22 -0500

    Update streamlit_app.py
    removed response text

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ try:
 except URLError as e:
   streamlit.error()
   
-streamlit.write('The user entered ', fruit_choice)
+# streamlit.write('The user entered ', fruit_choice)
 
 # import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
