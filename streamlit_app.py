a349d104f7a0abc22e94b9e643d710ca6e5d76c8
Yolanda <116392247+yolandadhoward28@users.noreply.github.com>
2022-11-04 08:38:28 -0500

    Update streamlit_app.py
    add function and button to add the fruit name submissions

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,33 +38,34 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
 streamlit.header("The fruit load list contains:")
-#Snowflake-related fuctions
+# Snowflake-related fuctions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
     
-# Add a button to loa the fruit
+# Add a button to load the fruit
 if streamlit.button ('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
-    
-            
+              
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
 # Allow the end uers to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return ('Thanks for adding ', add_my_fruit)
+        
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
-# will not work
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+if streamlit.button('Add a Fruit to the List'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        back_from_function = insert_row_snowflake(add_my_fruit)
+        streamlit.text(back_from_function)
