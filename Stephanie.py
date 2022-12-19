f41787474a4ca854b63fe76be1007f482a0d9af9
Yolanda <116392247+yolandadhoward28@users.noreply.github.com>
2022-12-19 10:06:32 -0600

    Update Stephanie.py

--- Stephanie.py
+++ Stephanie.py
@@ -32,18 +32,7 @@ def get_fruityvice_data(this_fruit_choice):
     return fruityvice_normalized
 
 
-#New section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
 
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:                                            
-        streamlit.error("Please select a fruit to get information.")
-    else:
-        back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
-    except URLError as e:
-        streamlit.error()
         
 
 
