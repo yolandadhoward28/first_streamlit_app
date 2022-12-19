dea5d63a0afd9a2b2f3ef779b3f80ca3b2efebd7
Yolanda <116392247+yolandadhoward28@users.noreply.github.com>
2022-12-19 09:40:28 -0600

    Update Stephanie.py

--- Stephanie.py
+++ Stephanie.py
@@ -35,3 +35,14 @@ def get_fruityvice_datax(this_fruit_choice):
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:                                            
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
+        
+        
+
+
