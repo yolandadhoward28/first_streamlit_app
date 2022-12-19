98aee1c92e989511912c914993b119cab8704233
Yolanda <116392247+yolandadhoward28@users.noreply.github.com>
2022-12-19 09:56:39 -0600

    Update Stephanie.py

--- Stephanie.py
+++ Stephanie.py
@@ -42,7 +42,8 @@ try:
     else:
         back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
-        
+    except URLError as e:
+        streamlit.error()
         
 
 
