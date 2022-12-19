08cdfb2451b6eb611f64e94777d049ecbe45ad9a
Yolanda <116392247+yolandadhoward28@users.noreply.github.com>
2022-12-19 09:24:55 -0600

    Update Stephanie.py

--- Stephanie.py
+++ Stephanie.py
@@ -41,7 +41,7 @@ try:
         back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
 #don't run anything past here while we troubleshoot
-#streamlit.stop()
+streamlit.stop()
 #import snowflake.connector
 
 streamlit.header("The fruit load list contains:")
