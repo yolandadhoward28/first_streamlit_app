e4dd7f083f0604efb50ee48b94e06cc1fa112f54
Yolanda <116392247+yolandadhoward28@users.noreply.github.com>
2022-12-19 09:29:59 -0600

    Update Stephanie.py

--- Stephanie.py
+++ Stephanie.py
@@ -31,40 +31,4 @@ def get_fruityvice_datax(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
-#New section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:                                            
-        streamlit.error("Please select a fruit to get information.")
-    else:
-        back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
-#don't run anything past here while we troubleshoot
-streamlit.stop()
-#import snowflake.connector
 
-streamlit.header("The fruit load list contains:")
-#Snowflake-related functions
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-        return my_cur.fetchall()
-
-# Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
-
-#my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-
-
-#streamlit.dataframe(my_data_rows)
-
-#allow the end user to add a fruit to the list
-#add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-#streamlit.write('Thanks for adding ', add_my_fruit)
-
-#This will not work correctly, but just go with it for now
-#my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
