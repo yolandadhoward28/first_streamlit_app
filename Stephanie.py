208b5d065b8d3c649f39c0c946ed52fa8ebd4c42
Yolanda <116392247+yolandadhoward28@users.noreply.github.com>
2022-12-19 09:38:28 -0600

    Update Stephanie.py

--- Stephanie.py
+++ Stephanie.py
@@ -31,3 +31,18 @@ def get_fruityvice_datax(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
+
+#New section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:                                            
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
+        
+        
+#don't run anything past here while we troubleshoot
+streamlit.stop()
+#import snowflake.connector
