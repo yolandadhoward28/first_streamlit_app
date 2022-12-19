9248c83ad254cacb33af2fedd01f097965338cc1
Yolanda <116392247+yolandadhoward28@users.noreply.github.com>
2022-12-19 09:20:03 -0600

    Update Stephanie.py

--- Stephanie.py
+++ Stephanie.py
@@ -7,7 +7,7 @@ from urllib.error import URLError
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text('?? Omega 3 & Blueberry Oatmeal')              10
+streamlit.text('?? Omega 3 & Blueberry Oatmeal')              
 streamlit.text('?? Kale, Spinach & Rocket Smoothie')
 streamlit.text('?? Hard-Boiled Free-Range Egg')
 streamlit.text('???? Advocado Toast')
@@ -35,7 +35,7 @@ def get_fruityvice_datax(this_fruit_choice):
 streamlit.header('Fruityvice Fruit Advice!')
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:                                            40
+    if not fruit_choice:                                            
         streamlit.error("Please select a fruit to get information.")
     else:
         back_from_function = get_fruityvice_data(fruit_choice)
